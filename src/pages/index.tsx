efb4a187699165e49325878a127dae90a821e3bc
Guilherme Borges <guilhermepardi@gmail.com>
2023-04-12 01:13:10 -0300

    Configurando stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,25 @@
+import { styled } from "../styles"
+
+const Button = styled("button", {
+  backgroundColor: '$purple',
+  border: '0',
+  borderRadius: '4px',
+  padding: '4px 8px',
+
+  'span': {
+    fontWeight: 'bold'
+  },
+
+  '&:hover': {
+    padding: '24px 28px'
+  }
+})
+
 export default function Home() {
-  return <h1>Hello</h1>
+  return (
+    <Button>
+      <span>Teste</span>
+      Enviar
+    </Button>
+  ) 
 }
