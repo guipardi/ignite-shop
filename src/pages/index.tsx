57ea5b62302554fd50890413146f5c00d2d4c5c7
Guilherme Borges <guilhermepardi@gmail.com>
2023-04-14 16:45:49 -0300

    Subindo navegação com Component Link do react

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { useKeenSlider } from "keen-slider/react"
 import { GetStaticProps } from "next";
 import { stripe } from "../lib/stripe";
 import { Stripe } from "stripe"
+import Link from "next/link"
 
 import Image from "next/image"
 import 'keen-slider/keen-slider.min.css'
@@ -29,14 +30,19 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => {
         return (
-          <Product className="keen-slider__slide" key={product.id}>
-            <Image src={product.imageUrl} width={520} height={480} alt=""/>
+          <Link 
+            key={product.id} 
+            href={`product/${product.id}`}
+          >
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt=""/>
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
