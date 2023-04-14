7310c086ef7fb2e925d739cba5d2242f107d8580
Guilherme Borges <guilhermepardi@gmail.com>
2023-04-14 17:12:27 -0300

    Subindo estrutura da página do produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,23 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/src/styles/pages/product"
 import { useRouter } from "next/router"
 
 export default function Product() {
   const { query } = useRouter()
 
   return  (
-    <h1>Produto: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+        
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>79, 90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea modi, repellat, voluptas deleniti quia eos quo magnam soluta maxime incidunt quaerat quidem earum, facilis id tempore esse? Ipsum, porro accusamus.</p>
+
+        <button>Comprar Agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
