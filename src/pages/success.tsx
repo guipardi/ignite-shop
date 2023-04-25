f4f8b1c9f027ec3df111a716d3107e33ebdc5dcf
Guilherme Borges <guilhermepardi@gmail.com>
2023-04-25 19:01:44 -0300

    Subindo a estrutura da página success
    Sem passar os dados reais da compras

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
+import { ImageContainer } from "../styles/pages/success";
+import { SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
   return (
-    <h1>Sucesso</h1>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhull <strong>Guilherme Pardi Borges</strong>, sua <strong>camiseta Beyond the Limits</strong> já está a caminha de sua casa
+      </p>
+
+      <Link href='/'>Voltar ao catálogo</Link>
+    </SuccessContainer>
   )
 }
