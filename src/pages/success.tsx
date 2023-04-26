2073be7f4e3c3481f17e4cf90789a1423ab6a75b
Guilherme Borges <guilhermepardi@gmail.com>
2023-04-26 15:39:24 -0300

    Subindo a parte de carregar informações na página de sucesso
    Usei um getServerSideProps par acessar os dados que foram pegos pelo id passado pela url, que foi mandado pela api, lá no checkout.ts

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,21 +1,54 @@
 import Link from "next/link";
 import { ImageContainer } from "../styles/pages/success";
 import { SuccessContainer } from "../styles/pages/success";
+import { GetServerSideProps } from "next";
+import { stripe } from "../lib/stripe";
+import Image from "next/image";
+import Stripe from "stripe";
 
-export default function Success() {
+interface SuccessProps {
+  name: string,
+  product: {
+    name: string,
+    image: string
+  }
+}
+
+export default function Success({ name, product }: SuccessProps) {
   return (
     <SuccessContainer>
       <h1>Compra efetuada!</h1>
 
       <ImageContainer>
-
+        <Image alt="" src={product.image} width={120} height={110}/>
       </ImageContainer>
 
       <p>
-        Uhull <strong>Guilherme Pardi Borges</strong>, sua <strong>camiseta Beyond the Limits</strong> já está a caminha de sua casa
+        Uhull <strong>{name}</strong>, sua <strong>{product.name}</strong> já está a caminha de sua casa
       </p>
 
       <Link href='/'>Voltar ao catálogo</Link>
     </SuccessContainer>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const sessionId = String(query.session_id)
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product']
+  })
+
+  const product = session.line_items?.data[0].price?.product as Stripe.Product
+  const customerName = session.customer_details?.name
+
+  return {
+    props: {
+      name: customerName,
+      product: {
+        name: product.name,
+        image: product.images[0]
+      }
+    }
+  }
 }
