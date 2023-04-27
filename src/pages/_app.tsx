e8b50486e7043a8eaf0eb77c14935ab89f4ec4fc
Guilherme Borges <guilhermepardi@gmail.com>
2023-04-27 10:02:38 -0300

    Subindo ícone do 'carrinho'
    Já estilizado

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 
 import logoImg from '../assets/logo.svg'
 import { Container, Header } from "../styles/pages/app"
+import { Tote } from "phosphor-react"
 
 globalStyles()
 
@@ -12,6 +13,9 @@ export default function App({ Component, pageProps }: AppProps) {
     <Container>
       <Header>
         <Image src={logoImg} alt=""/>
+        <div>
+          <Tote size={24}/>
+        </div>
       </Header>
       <Component {...pageProps} />
     </Container>
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -12,5 +12,19 @@ export const Header = styled('header', {
   padding: '2rem 0',
   width: '100%',
   maxWidth: '1180px',
-  margin: '0 auto'
+  margin: '0 auto',
+
+  display: 'flex',
+  justifyContent: 'space-between',
+
+  div: {
+    width: '48px',
+    height: '48px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '$gray800',
+    borderRadius: '6px',
+    cursor: 'pointer',
+  }
 })
